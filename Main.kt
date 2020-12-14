a241c0469123a7f45b152b47e1504d0148140a0f
dimasaditya26 <75971861+dimasaditya26@users.noreply.github.com>
2020-12-14 15:52:34 +0700

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+    val dimas : Human = Human("Andang Dimas Aditya Pratama")
+    val andi : Human = Human("Andi")
+
+     aditya.email = "adityadimas266@gmail.com"
+     aditya.address = "kediri, Jawa Timur"
+
+    andi.email = "andi@gmail.com"
+    andi.address = "Kotagede, Yogyakarta"
+
+    aditya.talk()
+    andi.talk()
+
+     aditya.introduce()
+
+    println( aditya.verify("adityadimas266@gmail.com"))
+
+    println(Rahardi.name)
+
+    val ardi : Rahardi = Rahardi
+    println(ardi.nationality)
+
+    println(aditya)
+}
